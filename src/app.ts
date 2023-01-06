e0479c950e42e67ca972782d689bba2f3920ab93
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2023-01-06 20:05:47 +0200

    4

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,3 @@ function merge<T extends object, U extends object> (objA: T , objB: U) {
   return Object.assign(objA, objB);
 }
 
-class Component {
-  constructor (public props:T) {
-
-  }
-}
-
-class Page extends Component {
-  pageInfo () {
-    console.log(this.props.title);
-  }
-}
