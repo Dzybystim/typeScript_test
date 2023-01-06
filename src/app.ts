a73f910e29cd59077ca0bcf19389944029cc73c2
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2023-01-06 16:01:18 +0200

    2

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,15 @@
-let age: number = 50;
-const fru: string = "Mama"
+
+ class Key {
+
+ }
+
+
+
+abstract class Plane {
+    door: boolean
+    key = new Key()
+
+    constructor (key: boolean) {
+        this.key = key;
+      }
+}
